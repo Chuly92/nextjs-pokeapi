278a1fc7757a0c926d5eca71a465d4b82f80dcd0
Julieta <julietagallego92@gmail.com>
2023-04-18 11:05:36 -0300

    end section 6

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -1,16 +1,21 @@
-import { ParsedUrlQuery } from "querystring";
-import pokeApi from "../api/pokeApi";
-import { Pokemon } from "../interfaces";
+import pokeApi from "../api/pokeApi"
+import { Pokemon } from "../interfaces"
 
 export const getPokemonInfo = async ( nameOrID: string) => {
   
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrID}`);
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrID}`)
 
-  return {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
+    return {
+      id: data.id,
+      name: data.name,
+      sprites: data.sprites,
+    }
+    
+  } catch (error) {
+    return null;
+  }
+
 };
 
 
